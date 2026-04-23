dad4275bce5443152ffcd129317b6da2675fd584
camel-1984 <rostmolchanov@mail.ru>
2026-04-23 15:05:17 +0300

    1

--- lab4/queries/dynamic_masking.sql
+++ lab4/queries/dynamic_masking.sql
@@ -19,7 +19,6 @@ IS 'MASKED WITH FUNCTION anon.partial_email(email)';
 SECURITY LABEL FOR anon ON COLUMN employees.phone
 IS 'MASKED WITH FUNCTION anon.partial(phone,2,$$******$$,2)';
 
--- Masking rules on base tables do not propagate to views automatically.
 SECURITY LABEL FOR anon ON COLUMN v_order_details.user_name
 IS 'MASKED WITH VALUE $$CONFIDENTIAL$$';
 
@@ -41,10 +40,3 @@ SECURITY LABEL FOR anon ON ROLE analyst_masked IS 'MASKED';
 GRANT CONNECT ON DATABASE db TO analyst_masked;
 GRANT USAGE ON SCHEMA public TO analyst_masked;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO analyst_masked;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO analyst_masked;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT SELECT ON TABLES TO analyst_masked;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT SELECT ON SEQUENCES TO analyst_masked;
